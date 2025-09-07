604fc1a71a9bbe758519a0c7111b8927b96c0c70
Veniamin Stakhovsky <vstakhovsky@users.noreply.github.com>
2025-09-07 19:23:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true, serverActions: true },
+  // experimental: { typedRoutes: true }  ← удаляем эту строку
+  // и заодно удаляем serverActions, если остался:
+  // experimental: { serverActions: true } ← тоже удалить
 };
 export default nextConfig;
